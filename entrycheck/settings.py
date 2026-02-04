128ba44422f81504b7a54d8dca72e1405ef74036
swethadomatoti <swethadomatoti@gmil.com>
2026-02-04 18:27:05 +0530

    Changes in settings

--- entrycheck/settings.py
+++ entrycheck/settings.py
@@ -148,6 +148,7 @@ USE_I18N = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
