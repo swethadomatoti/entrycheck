0ff6d4205f17de677828ef6dae128efa016de985
swethadomatoti <swethadomatoti@gmil.com>
2026-02-04 15:57:42 +0530

    add database in settings

--- entrycheck/settings.py
+++ entrycheck/settings.py
@@ -99,11 +99,16 @@ WSGI_APPLICATION = 'entrycheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url   
+import os  
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,    
+        conn_max_age=600,        
+        ssl_require=True        
+    )
 }
 
 
