e2f245b833076dd7808c842098328ec0aa51bc93
swethadomatoti <swethadomatoti@gmil.com>
2026-03-03 12:57:20 +0530

    Updated requirements

--- entrycheck/settings.py
+++ entrycheck/settings.py
@@ -101,18 +101,16 @@ WSGI_APPLICATION = 'entrycheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url   
-import os  
-DATABASE_URL = os.environ.get("DATABASE_URL")
+import dj_database_url
+import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=DATABASE_URL,    
-        conn_max_age=600,        
-        ssl_require=True        
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
